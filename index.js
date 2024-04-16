8ea71ed99422fba15800aa52d756886e77a6cc08
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-04-16 13:44:38 +0300

    installed express & send simple response

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('There are 42 portcalls');
+})
+
 console.log("There are 42 portcalls");
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
