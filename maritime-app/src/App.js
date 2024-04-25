fa7509311fb5c7c8b9cbec510ed2454932e07d46
Jingjing Yang <jingjing.yang@tuni.fi>
2024-04-25 13:50:25 +0300

    add ETA table

--- maritime-app/src/App.js
+++ maritime-app/src/App.js
@@ -1,29 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { initiateSocketConnection, disconnectSocket, subscribeToUpdates } from './socketServer';
+import React, { useEffect, useState } from "react";
+import {
+  initiateSocketConnection,
+  disconnectSocket,
+  subscribeToUpdates,
+} from "./socketServer";
+
+import { Container } from "@mui/material";
+import PortETA from "./components/PortETA";
 
 function App() {
-    const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState([]);
 
-    useEffect(() => {
-        initiateSocketConnection();
+  useEffect(() => {
+    initiateSocketConnection();
 
-        subscribeToUpdates((newMessage) => {
-            setNotifications(prevNotifications => [...prevNotifications, newMessage]);
-        });
+    subscribeToUpdates((newMessage) => {
+      setNotifications((prevNotifications) => [
+        ...prevNotifications,
+        newMessage,
+      ]);
+    });
 
-        return () => {
-            disconnectSocket();
-        };
-    }, []);
+    return () => {
+      disconnectSocket();
+    };
+  }, []);
 
-    return (
-        <div>
-            <h1>Notifications</h1>
-            {notifications.map((note, index) => (
-                <div key={index}>{note}</div>
-            ))}
-        </div>
-    );
+  const [portCalls, setPortCalls] = useState([]);
+
+  useEffect(() => {
+    fetch("https://meri.digitraffic.fi/api/port-call/v1/port-calls")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPortCalls(data.portCalls || []);
+        console.log(`There are ${data.portCalls.length} portcalls.`);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch port calls:", error);
+      });
+  }, []);
+
+  return (
+    <Container style={{ margin: "30px auto" }}>
+      <PortETA portCalls={portCalls} />
+      <h1>Notifications</h1>
+      {notifications.map((note, index) => (
+        <div key={index}>{note}</div>
+      ))}
+    </Container>
+  );
 }
 
 export default App;
---
+++ maritime-app/src/components/PortETA.js
@@ -0,0 +1,104 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import Link from "@mui/material/Link"; // Import Link component from MUI
+
+export default function PortETA({ portCalls }) {
+  const columns = [
+    {
+      field: "vesselName",
+      headerName: "Vessel",
+      width: 150,
+    },
+    {
+      field: "nextPort",
+      headerName: "Next Port",
+      width: 150,
+    },
+    {
+      field: "ata",
+      headerName: "Actual Arrival Time",
+      width: 200,
+      sortComparator: (v1, v2) => {
+        // Custom comparator to handle null values and compare dates
+        if (v1 === "null") return 1;
+        if (v2 === "null") return -1;
+        const date1 = new Date(v1);
+        const date2 = new Date(v2);
+        return date1 - date2;
+      },
+    },
+    {
+      field: "eta",
+      headerName: "Estimated Arrival Time",
+      width: 200,
+    },
+    {
+      field: "location",
+      headerName: "Location",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <Link href={params.value} target="_blank" rel="noopener noreferrer">
+            View Location
+          </Link>
+        );
+      },
+    },
+  ];
+
+  const rows = portCalls.map((portCall) => {
+    let ata = portCall.portAreaDetails[0].ata;
+    let eta = portCall.portAreaDetails[0].eta;
+
+    let ataStr = "";
+    let etaStr = "";
+
+    if (ata && typeof ata === "string") {
+      ataStr = ata.split(".")[0].replace("T", " ");
+    } else {
+      ataStr = "null";
+    }
+
+    if (eta && typeof eta === "string") {
+      etaStr = eta.split(".")[0].replace("T", " ");
+    } else {
+      etaStr = "null";
+    }
+
+    return {
+      id: portCall.portCallId,
+      vesselName: portCall.vesselName,
+      nextPort: portCall.nextPort,
+      ata: ataStr,
+      eta: etaStr,
+      location: `https://baltice.org/map/ships/${portCall.radioCallSign}`,
+    };
+  });
+
+  return (
+    <Box sx={{ height: 400 }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          sorting: {
+            sortModel: [
+              {
+                field: "ata",
+                sort: "desc", // descending order
+              },
+            ],
+          },
+          pagination: {
+            paginationModel: {
+              pageSize: 100,
+            },
+          },
+        }}
+        pageSizeOptions={[5, 10, 20]}
+        checkboxSelection
+      />
+    </Box>
+  );
+}
